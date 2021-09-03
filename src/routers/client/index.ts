6ca376f9f8cd24de5c78ce72ca01fc6b095e7c11
Lucas de Morais <lucasmoraismt@gmail.com>
2021-09-02 23:55:44 -0300

    feat: get purchase initial route

--- src/routers/client/index.ts
+++ src/routers/client/index.ts
@@ -4,6 +4,7 @@ import eventRouter from "@/routers/client/event";
 import userRouter from "@/routers/client/user";
 import authRouter from "@/routers/client/auth";
 import enrollmentRouter from "@/routers/client/enrollment";
+import purchaseRouter from "@/routers/client/purchase";
 
 import tokenValidationMiddleware from "@/middlewares/tokenValidationMiddleware";
 
@@ -13,5 +14,6 @@ router.use("/event", eventRouter);
 router.use("/users", userRouter);
 router.use("/auth", authRouter);
 router.use("/enrollments", tokenValidationMiddleware, enrollmentRouter);
+router.use("/purchase", purchaseRouter);
 
 export default router;
